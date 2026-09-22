699d1389339f7f81f65a9f98af933bed25700097
Naygaonkar Kedar Deepak <kedarn2006@gmail.com>
2026-09-22 18:46:35 +0530

    Clean up commented code in count-values-with-equally-spaced-occurrences-i.java
    Removed commented-out code related to frequency counting.

--- 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.java
+++ 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.java
@@ -32,33 +32,7 @@ class Solution {
                 }
             }
         }
-        // for(int i=0;i<nums.length-1;i++){
-        //     int freq_count=0;
-        //     int i1=0;
-        //     int i2=0;
-        //     int i3=0;
-        //     for(int j=i+1;j<nums.length;j++){
-        //         if(nums[i]==nums[j] || freq_count<=3){
-        //             freq_count++;
-        //             i1=i;
-        //             if(freq_count==2){
-        //                 i2=j;
-        //             }
-        //             if(freq_count==3){
-        //                 i3=j;
-        //             }
-        //             if(i3-i2==i2-i1){
-        //                 freq[nums[i]]+=3;
-        //             }
-        //         }
-        //     }
-            
-        // }
-        // for(int i=0;i<freq.length;i++){
-        //     if(freq[i]==3){
-        //         special++;
-        //     }
-        // }
+        
         return special;
     }
 }
